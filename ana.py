b18dd0ae7a7141998ac44308177fcee7778c65ec
aharikri <55405542+aharikri@users.noreply.github.com>
2020-11-03 16:09:41 +0530

    Update ana.py

--- ana.py
+++ ana.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/') 
 def hello(): 
-	return "welcome to the Oracle web app with s python in DevSecOPs this is change new  !! Modified modified2"
+	return "we1lcome to the Oracle web app with s python in DevSecOPs this is change new  !! Modified modified2"
 
 
 if __name__ == "__main__": 
